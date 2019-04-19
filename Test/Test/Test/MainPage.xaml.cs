dd43f6f00c17521f9694139fd1853d9bc94e95e3
Jarrod <jl727@uowmail.edu.au>
2019-04-19 10:34:52 +1000

    Test app for display

---
+++ Test/Test/Test/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Test
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var tgr = new TapGestureRecognizer();
+            tgr.Tapped += (s, e) => OnLabelClicked();
+
+            Click_Text.GestureRecognizers.Add(tgr);
+
+        }
+
+        public async void Next_Page(Object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Second_Page());
+        }
+
+        public async void OnLabelClicked()
+        {
+           var action = await DisplayAlert("Clicked", "Did you click the text?", "Yes", "No");
+            if (action)
+            {
+                Change_Label.Text = "You changed me!";
+            }
+            else
+            {
+                await DisplayAlert("error", "You chose no..", "Ok");
+            }
+        }
+    }
+}
---
+++ Test/Test/Test/Second_Page.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Test
+{
+    public partial class Second_Page : ContentPage
+    {
+        public Second_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
