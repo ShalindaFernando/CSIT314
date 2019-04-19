a2da17aea3117e413d366d826afcabc512cf9a82
Jarrod <jl727@uowmail.edu.au>
2019-04-19 10:30:51 +1000

    Added Users text file + reading in for it

--- Group13/Group13/MainPage.xaml.cs
+++ Group13/Group13/MainPage.xaml.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 using Xamarin.Forms;
 
 namespace Group13
@@ -11,10 +13,12 @@ namespace Group13
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(true)]
+
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
+
             InitializeComponent();
 
             Task.Run(() => { LoadData(); });
@@ -22,10 +26,21 @@ namespace Group13
             Navigate();
         }
 
-        private async void LoadData()
+        private void LoadData()
         {
             //Load Users Information into Array
-            await Task.Run(() => { UserData.GetUserDetails();});
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("Group13.UsersData.txt");
+            string text = "";
+
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+            System.Diagnostics.Debug.WriteLine("Reading text file.");
+            System.Diagnostics.Debug.WriteLine(text);
+
+            //Permutate "text" variable to allow to be added to array.
         }
 
         private async void Navigate()
