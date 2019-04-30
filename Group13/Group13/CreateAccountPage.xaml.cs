a9b73804bc7d6ed47391309b5fa711edc7d95d2b
Shalinda Fernando <>
2019-05-01 03:01:18 +1000

    Started the Create Account Page (CreateAccountPage)

---
+++ Group13/Group13/CreateAccountPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Group13
+{
+    public partial class CreateAccountPage : ContentPage
+    {
+        public CreateAccountPage()
+        {
+            InitializeComponent();
+        }
+
+        void SignUpButton_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
