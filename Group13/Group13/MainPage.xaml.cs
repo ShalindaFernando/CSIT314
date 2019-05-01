c594d216b3f383e3156ca5cb4a52f306d139b6a1
Jarrod <jl727@uowmail.edu.au>
2019-05-01 17:09:43 +1000

    First page of Registration process

--- Group13/Group13/MainPage.xaml.cs
+++ Group13/Group13/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +15,14 @@ namespace Group13
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(true)]
 
+
     public partial class MainPage : ContentPage
     {
+        List<Users> UserArray = new List<Users>(50);
+
         public MainPage()
         {
+
 
             InitializeComponent();
 
@@ -33,13 +38,36 @@ namespace Group13
             Stream stream = assembly.GetManifestResourceStream("Group13.UsersData.txt");
             string text = "";
 
+                   // System.Diagnostics.Debug.WriteLine("Reading text file.");
+
             using (var reader = new System.IO.StreamReader(stream))
             {
-                text = reader.ReadLine();
-                //text = reader.ReadToEnd();
+                //text = reader.ReadLine();
+                text = reader.ReadToEnd();
+
+                while (text != null)
+                {
+                    var parts = text.Split(',');
+                    Users user = new Users();
+
+                    user.userId = Convert.ToInt32(parts[0]);
+                    user.fName = parts[1];
+                    user.lName = parts[2];
+                    user.email = parts[3];
+                    user.userType = parts[4];
+                    user.subscription = parts[5];
+                    user.carMake = parts[6];
+                    user.carModel = parts[7];
+                    user.carColour = parts[8];
+                    user.registration = parts[9];
+                    user.transmission = Convert.ToBoolean(parts[10]);
+                    user.cylinders = Convert.ToInt32(parts[11]);
+
+                    UserArray.Add(user);
+                    Console.WriteLine(user.print());
+                }
+
             }
-            System.Diagnostics.Debug.WriteLine("Reading text file.");
-            System.Diagnostics.Debug.WriteLine(text);
 
             //Permutate "text" variable to allow to be added to array.
         }
---
+++ Group13/Group13/Users.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Group13
+{
+    public class Users
+    {
+        public int userId { get; set; }
+        public String fName { get; set; }
+        public String lName { get; set; }
+        public String email { get; set; }
+        public String userType { get; set; }
+        public String subscription { get; set; }
+        public String carMake { get; set; }
+        public String carModel { get; set; }
+        public String carColour { get; set; }
+        public String registration { get; set; }
+        public bool transmission { get; set; }
+        public int cylinders { get; set; }
+
+        public Users()
+        {
+
+        }
+
+        public Users(int userId, string fName, string lName, string email, string userType, string subscription, string carMake, string carModel, string carColour, string registration, bool transmission, int cylinders)
+        {
+            this.userId = userId;
+            this.fName = fName;
+            this.lName = lName;
+            this.email = email;
+            this.userType = userType;
+            this.subscription = subscription;
+            this.carMake = carMake;
+            this.carModel = carModel;
+            this.carColour = carColour;
+            this.registration = registration;
+            this.transmission = transmission;
+            this.cylinders = cylinders;
+        }
+
+        public string print ()
+        {
+            return (fName + " " + lName);
+        }
+    }
+}
